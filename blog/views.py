57cdf8091755ff38ebd9154d9de882b3ebc5668a
Leica-HP_PC <hy19900815work@126.com>
2017-10-13 10:22:12 +0800

    blog details

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -9,4 +9,8 @@ def index(request):
         'post_list': post_list
     }
                   )
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', context={'post': post})
 
