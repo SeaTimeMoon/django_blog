60b2a561c8049e103685114a35b1053c6310a305
Carl Hu <hy19900815work@126.com>
2017-10-15 13:36:38 +0800

    add comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from comments.forms import CommentForm
 from .models import Post, Category
 
 # Create your views here.
@@ -19,7 +20,13 @@ def detail(request, pk):
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
 
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
